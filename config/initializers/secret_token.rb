570c9be91e70daf22ff301c1f35bf47374fd596f
yatinla <mike@taylorwebhome.org>
2012-11-10 12:53:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3d94ed497c1f4d408b2ea25cfa894c6645f65cc7f0ee8440c80b814787428c8915e72d7b225f2d598352d9ac90520ee8230009572c552e3943d01af1908b170c'
